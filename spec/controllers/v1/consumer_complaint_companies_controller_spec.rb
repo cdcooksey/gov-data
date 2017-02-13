b33b6b0b258737c0fa71118ff6da2c0fdf908731
Charlie Cooksey <ccooksey@bbasolutions.com>
2017-02-13 16:01:17 -0700

    last change of the day

--- spec/controllers/v1/consumer_complaint_companies_controller_spec.rb
+++ spec/controllers/v1/consumer_complaint_companies_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ConsumerComplaintCompaniesController, type: :controller do
+RSpec.describe V1::ConsumerComplaintCompaniesController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
