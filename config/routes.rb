cdabbed72eee3d48305f203d1900bdb6ff2ccfa9
Charlie Cooksey <ccooksey@bbasolutions.com>
2017-02-13 10:37:17 -0700

    Fixes #3

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :consumer_complaints, path: 'consumer-complaints', only: [:index, :show], defaults: { format: :json }
     resources :consumer_complaint_companies, path: 'consumer-complaint-companies', only: [:index, :show], defaults: { format: :json }
     resources :consumer_complaint_products, path: 'consumer-complaint-products', only: [:index, :show], defaults: { format: :json }
+    resources :consumer_complaint_subproducts, path: 'consumer-complaint-subproducts', only: [:index, :show], defaults: { format: :json }
   end
 
 end
