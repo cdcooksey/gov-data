d8a7e1d627b5740caceef975c7e822e201a09573
Charlie Cooksey <ccooksey@bbasolutions.com>
2017-02-16 11:12:30 -0700

    adds controller method and route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # Always version the API
   namespace :v1 do
     resources :consumer_complaints, path: 'consumer-complaints', only: [:index, :show], defaults: { format: :json }
+
     resources :consumer_complaint_companies, path: 'consumer-complaint-companies', only: [:index, :show], defaults: { format: :json }
+    get 'consumer-complaint-companies/:id/consumer-complaints' => 'consumer_complaint_companies#consumer_complaints', defaults: { format: :json }, as: :v1_consumer_complaint_company_consumer_complaints
+
     resources :consumer_complaint_products, path: 'consumer-complaint-products', only: [:index, :show], defaults: { format: :json }
     resources :consumer_complaint_subproducts, path: 'consumer-complaint-subproducts', only: [:index, :show], defaults: { format: :json }
     resources :consumer_complaint_issues, path: 'consumer-complaint-issues', only: [:index, :show], defaults: { format: :json }
