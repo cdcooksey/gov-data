2630cd77a8e8f1e328489f9c50f8a741742d9d18
Charlie Cooksey <ccooksey@bbasolutions.com>
2017-02-16 19:35:42 -0700

    remove company data from complaint serializer

--- app/serializers/consumer_complaint_serializer.rb
+++ app/serializers/consumer_complaint_serializer.rb
@@ -1,13 +1,5 @@
 class ConsumerComplaintSerializer < ActiveModel::Serializer
-  attributes :id, :complaint_id, :date_received, :date_sent_to_company, :timely_response, :consumer_disputed, :submitted_via, :company_name, :company_id
-
-  def company_name
-    object.consumer_complaint_company.name
-  end
-
-  def company_id
-    object.consumer_complaint_company.id
-  end
+  attributes :id, :complaint_id, :date_received, :date_sent_to_company, :timely_response, :consumer_disputed, :submitted_via
 
   belongs_to :consumer_complaint_company
   belongs_to :consumer_complaint_product
