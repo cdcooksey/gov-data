014416d41f142d03b606be413cc9dc34f513190a
Charlie Cooksey <cdcooksey@gmail.com>
2018-06-10 11:24:58 -0600

    removes pluarl `ActiveModelSerializer` classes

--- app/serializers/consumer_complaint_products_serializer.rb
+++
@@ -1,4 +0,0 @@
-class ConsumerComplaintProductsSerializer < ActiveModel::Serializer
-  attributes :id, :name
-  has_many :consumer_complaints
-end
--- app/serializers/consumer_complaint_zip_codes_serializer.rb
+++
@@ -1,4 +0,0 @@
-class ConsumerComplaintZipCodesSerializer < ActiveModel::Serializer
-  attributes :id, :name
-  has_many :consumer_complaints
-end
