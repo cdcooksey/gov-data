037a1d9ac82c32dee597f413101eecacfe4a7c99
Charlie Cooksey <cdcooksey@gmail.com>
2018-06-10 10:50:46 -0600

    fixes `spec/controllers/v1/consumer_complaint_companies_controller_spec.rb`

--- spec/controllers/v1/consumer_complaint_companies_controller_spec.rb
+++ spec/controllers/v1/consumer_complaint_companies_controller_spec.rb
@@ -10,11 +10,16 @@ RSpec.describe V1::ConsumerComplaintCompaniesController, type: :controller do
   end
 
   describe "GET #show" do
+    let(:company)    { FactoryBot.create(:consumer_complaint_company) }
+    let(:company_id) { company.id }
+
+    before { get :show, params: { id: company_id } }
+
     it "returns http success" do
-      get v1_consumer_complaint_company_url create(:consumer_complaint_company), format: :json
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
     end
   end
+
 
 end
 
