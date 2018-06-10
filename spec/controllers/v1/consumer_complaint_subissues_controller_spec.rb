ce035f12ab9518adfa81bb60f8a0194a615fc2d8
Charlie Cooksey <cdcooksey@gmail.com>
2018-06-10 10:49:10 -0600

    fixes `spec/controllers/v1/consumer_complaint_subissues_contr`

--- spec/controllers/v1/consumer_complaint_subissues_controller_spec.rb
+++ spec/controllers/v1/consumer_complaint_subissues_controller_spec.rb
@@ -10,8 +10,13 @@ RSpec.describe V1::ConsumerComplaintSubissuesController, type: :controller do
   end
 
   describe "GET #show" do
+
+    let(:subissue)    { FactoryBot.create(:consumer_complaint_subissue) }
+    let(:subissue_id) { subissue.id }
+
+    before { get :show, params: { id: subissue_id } }
+
     it "returns http success" do
-      get :show
       expect(response).to have_http_status(:ok)
     end
   end
