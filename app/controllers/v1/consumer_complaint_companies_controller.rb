1a76f7df38b7806685689836bfb16de2b663695b
Charlie Cooksey <cdcooksey@gmail.com>
2018-06-21 10:44:53 -0600

    updates `ConsumerComplaintCompaniesController` to use `ConsumerComplaint#includes()` to speed up rendering

--- app/controllers/v1/consumer_complaint_companies_controller.rb
+++ app/controllers/v1/consumer_complaint_companies_controller.rb
@@ -14,6 +14,12 @@ class V1::ConsumerComplaintCompaniesController < V1::ApplicationController
 
   def paginated_complaints
     ConsumerComplaint
+      .includes(:consumer_complaint_company,
+                :consumer_complaint_product,
+                :consumer_complaint_subproduct,
+                :consumer_complaint_issue,
+                :consumer_complaint_subissue,
+                :consumer_complaint_zip_code)
       .where(consumer_complaint_company_id: consumer_complaint_company_params[:id])
       .offset(offset)
       .limit(PAGE_LIMIT)
