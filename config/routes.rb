aa454b491424d8d959c79fb25d4a8194069ddd98
Charlie Cooksey <cdcooksey@gmail.com>
2018-06-21 20:33:09 -0600

    updates `config/routes.rb` to include zip code routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :consumer_complaints, path: 'consumer-complaints', only: [:index]
       resources :consumer_complaint_companies, path: 'consumer-complaint-companies', only: [:index, :show]
+      resources :consumer_complaint_zip_codes, path: 'consumer-complaint-zip-codes', only: [:index, :show], defaults: { format: :json }
 
       resources :consumer_complaint_products, path: 'consumer-complaint-products', only: [:index, :show], defaults: { format: :json }
       resources :consumer_complaint_subproducts, path: 'consumer-complaint-subproducts', only: [:index, :show], defaults: { format: :json }
       resources :consumer_complaint_issues, path: 'consumer-complaint-issues', only: [:index, :show], defaults: { format: :json }
       resources :consumer_complaint_subissues, path: 'consumer-complaint-subissues', only: [:index, :show], defaults: { format: :json }
-      resources :consumer_complaint_zip_codes, path: 'consumer-complaint-zip-codes', only: [:index, :show], defaults: { format: :json }
     end
 
   end
