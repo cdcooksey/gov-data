768434b8737e9ce720dcc08e7bca8a251b1f0ea1
Charlie Cooksey <cdcooksey@gmail.com>
2018-06-21 20:22:28 -0600

    updates `ConsumerComplaintZipCodesController#show` and adds spec

--- app/controllers/v1/consumer_complaint_zip_codes_controller.rb
+++ app/controllers/v1/consumer_complaint_zip_codes_controller.rb
@@ -1,11 +1,25 @@
 class V1::ConsumerComplaintZipCodesController < V1::ApplicationController
 
   def show
-    @consumer_complaint_zip_code = ConsumerComplaintZipCode.find_by(consumer_complaint_zip_code_params)
-    render json: @consumer_complaint_zip_code
+    res = ConsumerComplaintSerializer.new(paginated_complaints)
+    json_response(res.serializable_hash, :ok)
   end
 
   private
+
+
+  def paginated_complaints
+    ConsumerComplaint
+      .includes(:consumer_complaint_company,
+                :consumer_complaint_product,
+                :consumer_complaint_subproduct,
+                :consumer_complaint_issue,
+                :consumer_complaint_subissue,
+                :consumer_complaint_zip_code)
+      .where(consumer_complaint_zip_code_id: consumer_complaint_zip_code_params[:id])
+      .offset(offset)
+      .limit(PAGE_LIMIT)
+  end
 
   def consumer_complaint_zip_code_params
     params.permit(:id)
---
+++ spec/controllers/v1/consumer_complaint_zip_codes_controller_spec.rb
@@ -0,0 +1,95 @@
+require 'rails_helper'
+
+RSpec.describe V1::ConsumerComplaintZipCodesController, type: :controller do
+
+  let(:actual) { JSON.parse(response.body) }
+  subject { actual }
+
+#  describe "GET #index" do
+#    before do
+#      companies
+#      action
+#    end
+#
+#    let(:action) { get :index }
+#    let(:companies) { FactoryBot.create_list(:consumer_complaint_company, 103) }
+#
+#    let(:expected_payload) do
+#      JSON.parse(ConsumerComplaintCompanySerializer.new(expected_companies).serialized_json)
+#    end
+#
+#    context 'without page param' do
+#      let(:expected_companies) { companies.first(50) }
+#
+#      it { expect(response).to have_http_status :ok }
+#      it { is_expected.to eq(expected_payload) }
+#    end
+#
+#    context 'with page param' do
+#      let(:page) { 1 }
+#      let(:action) { get :index, params: { page: page } }
+#
+#      let(:expected_companies) { companies.drop(50).first(50) }
+#
+#      it { expect(response).to have_http_status :ok }
+#      it { is_expected.to eq(expected_payload) }
+#    end
+#
+#    context 'when no companies exist' do
+#      let(:companies) { [] }
+#      let(:expected_payload) { { data: [] }.as_json }
+#
+#      it { expect(response).to have_http_status :ok }
+#      it { is_expected.to eq(expected_payload) }
+#    end
+# 
+#  end
+
+
+  describe "GET #show" do
+    before do
+      expected_payload
+      action
+    end
+
+    let(:zip_code)    { FactoryBot.create(:consumer_complaint_zip_code) }
+    let(:zip_code_id) { zip_code.id }
+
+    let(:consumer_complaints) do 
+      FactoryBot.create_list(:consumer_complaint, 1, consumer_complaint_zip_code: zip_code)
+    end
+
+    let(:expected_payload) do
+      JSON.parse(ConsumerComplaintSerializer.new(expected_consumer_complaints).serialized_json)
+    end
+
+    let(:action) { get :show, params: { id: zip_code_id} }
+
+    context 'without page param' do
+      let(:expected_consumer_complaints) { consumer_complaints.first(50) }
+
+      it { expect(response).to have_http_status :ok }
+      it { is_expected.to eq(expected_payload) }
+    end
+
+    context 'with page param' do
+      let(:page) { 1 }
+      let(:action) { get :show, params: { id: zip_code_id, page: page } }
+      let(:expected_consumer_complaints) { consumer_complaints.drop(50).first(50) }
+
+      it { expect(response).to have_http_status :ok }
+      it { is_expected.to eq(expected_payload) }
+    end
+
+    context 'when no ConsumerComplaints exist' do
+      let(:consumer_complaints) { [] }
+      let(:expected_payload) { { data: [] }.as_json }
+
+      it { expect(response).to have_http_status :ok }
+      it { is_expected.to eq(expected_payload) }
+    end
+  end
+
+
+end
+
