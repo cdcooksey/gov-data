d2e34cec7e46b06e799a196c9b9d6cc40e84b04c
Charlie Cooksey <cdcooksey@gmail.com>
2018-06-21 20:26:43 -0600

    adds tests for `ConsumerComplaintZipCodesController#index`

--- spec/controllers/v1/consumer_complaint_zip_codes_controller_spec.rb
+++ spec/controllers/v1/consumer_complaint_zip_codes_controller_spec.rb
@@ -5,45 +5,45 @@ RSpec.describe V1::ConsumerComplaintZipCodesController, type: :controller do
   let(:actual) { JSON.parse(response.body) }
   subject { actual }
 
-#  describe "GET #index" do
-#    before do
-#      companies
-#      action
-#    end
-#
-#    let(:action) { get :index }
-#    let(:companies) { FactoryBot.create_list(:consumer_complaint_company, 103) }
-#
-#    let(:expected_payload) do
-#      JSON.parse(ConsumerComplaintCompanySerializer.new(expected_companies).serialized_json)
-#    end
-#
-#    context 'without page param' do
-#      let(:expected_companies) { companies.first(50) }
-#
-#      it { expect(response).to have_http_status :ok }
-#      it { is_expected.to eq(expected_payload) }
-#    end
-#
-#    context 'with page param' do
-#      let(:page) { 1 }
-#      let(:action) { get :index, params: { page: page } }
-#
-#      let(:expected_companies) { companies.drop(50).first(50) }
-#
-#      it { expect(response).to have_http_status :ok }
-#      it { is_expected.to eq(expected_payload) }
-#    end
-#
-#    context 'when no companies exist' do
-#      let(:companies) { [] }
-#      let(:expected_payload) { { data: [] }.as_json }
-#
-#      it { expect(response).to have_http_status :ok }
-#      it { is_expected.to eq(expected_payload) }
-#    end
-# 
-#  end
+  describe "GET #index" do
+    before do
+      expected_payload
+      action
+    end
+
+    let(:action) { get :index }
+    let(:zip_codes) { FactoryBot.create_list(:consumer_complaint_zip_code, 103) }
+
+    let(:expected_payload) do
+      JSON.parse(ConsumerComplaintZipCodeSerializer.new(expected_zip_codes).serialized_json)
+    end
+
+    context 'without page param' do
+      let(:expected_companies) { zip_codes.first(50) }
+
+      it { expect(response).to have_http_status :ok }
+      it { is_expected.to eq(expected_payload) }
+    end
+
+    context 'with page param' do
+      let(:page) { 1 }
+      let(:action) { get :index, params: { page: page } }
+
+      let(:expected_zip_codes) { zip_codes.drop(50).first(50) }
+
+      it { expect(response).to have_http_status :ok }
+      it { is_expected.to eq(expected_payload) }
+    end
+
+    context 'when no zip_codes exist' do
+      let(:companies) { [] }
+      let(:expected_payload) { { data: [] }.as_json }
+
+      it { expect(response).to have_http_status :ok }
+      it { is_expected.to eq(expected_payload) }
+    end
+
+  end
 
 
   describe "GET #show" do
